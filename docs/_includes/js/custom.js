13483c1ed048696e11f608c12b7be0ed93374edb
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-05-11 15:00:42 +0200

    remove redunant javascript

--- docs/_includes/js/custom.js
+++ docs/_includes/js/custom.js
@@ -1,9 +1,7 @@
 async function switchScheme(isClicked) {
   if (isClicked == true) {
     jtd.setTheme("eyetrackvr");
-    switch_scheme.textContent = "Light Mode";
   } else {
     jtd.setTheme("light");
-    switch_scheme.textContent = "Dark Mode";
   }
 }
