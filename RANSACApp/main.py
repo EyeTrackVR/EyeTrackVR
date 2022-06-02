ff722c79a2d252038a6c2e61312b6a60691f9640
Kyle Machulis <kyle@nonpolynomial.com>
2022-06-01 21:25:51 -0700

    Switch to using PySimpleGUI w/ tkinter
    Doesn't seem to cause processing issues, looks good, gives us text
    capabilities.

--- RANSACApp/main.py
+++ RANSACApp/main.py
@@ -5,24 +5,26 @@ from ransac import Ransac
 import queue
 import threading
 import cv2
+import PySimpleGUI as sg
 
 WINDOW_NAME = "RANSACApp"
+THRESHOLD_SLIDER_NAME = "THREADHOLDSLIDER"
+ROTATION_SLIDER_NAME = "ROTATIONSLIDER"
 
 def main():
   # Get Configuration
   config: RansacConfig = RansacConfig.load()
   config.save()
 
-  # Set up basic cv2 window with our GUI
-  def update_threshold(val: "int"):
-      config.threshhold = val
-  
-  def update_rot(val: "int"):
-      config.rotation_angle = val
-  
-  cv2.namedWindow(WINDOW_NAME)
-  cv2.createTrackbar("Threshold", WINDOW_NAME, 0, 100, update_threshold)
-  cv2.createTrackbar("Rotation", WINDOW_NAME, 0, 360, update_rot)
+  # Define the window's contents
+  layout = [[sg.Text("Threshold"), sg.Slider(range=(0, 100), orientation = 'h', key=THRESHOLD_SLIDER_NAME)],
+            [sg.Text("Rotation"), sg.Slider(range=(0, 360), orientation = 'h', key=ROTATION_SLIDER_NAME)],
+            [sg.Image(filename="", key="-IMAGE-")],
+            ]
+
+  # Create the window
+  window = sg.Window('Window Title', layout)
+
 
   # Spawn worker threads
   osc_queue: "queue.Queue[tuple[bool, int, int] | None]" = queue.Queue()
@@ -42,23 +44,15 @@ def main():
   ransac_thread = threading.Thread(target=ransac.run)
   ransac_thread.start()
 
-
   # GUI Render loop
 
   while True:
     # If we're in ROI mode, show current video and allow markup.
-
-    # If we're in tracking mode, bring up the tracking thread, let it do all of its work, then
-    # update ourselves whenever it pushes out an image into its buffer.
-    try:
-      maybe_image = image_queue.get(block = False)
-      cv2.imshow(WINDOW_NAME, maybe_image)
-    except queue.Empty:
-      pass
+    event, values = window.read(timeout=1)
 
     # If we're in either mode and someone hits q, quit immediately
-    if cv2.waitKey(10) & 0xFF == ord("q"):
-      cv2.destroyAllWindows()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+      # cv2.destroyAllWindows()
       osc_queue.put(None)
       osc_thread.join()
       ransac_queue.put(None)
@@ -68,6 +62,20 @@ def main():
 #      t2s_thread.join()
       print("Exiting RANSAC App")
       return
+
+    config.threshhold = values[THRESHOLD_SLIDER_NAME]
+    config.rotation_angle = values[ROTATION_SLIDER_NAME]
+
+    # If we're in tracking mode, bring up the tracking thread, let it do all of its work, then
+    # update ourselves whenever it pushes out an image into its buffer.
+    try:
+      maybe_image = image_queue.get(block = False)
+      imgbytes = cv2.imencode(".ppm", maybe_image)[1].tobytes()
+      window["-IMAGE-"].update(data=imgbytes)
+      # cv2.imshow(WINDOW_NAME, maybe_image)
+    except queue.Empty:
+      pass
+
 
 if __name__ == "__main__":
   main()
