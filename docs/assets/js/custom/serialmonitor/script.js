3d299f44dbcb3b8e36f5365f2d39a02fde92a175
DaOfficialWizard <pyr0ndet0s97@gmail.com>
2022-07-01 23:28:33 +0100

    add contributors easter egg add logo svg to footer

--- docs/assets/js/custom/serialmonitor/script.js
+++ docs/assets/js/custom/serialmonitor/script.js
@@ -5,7 +5,17 @@ var port,
   historyIndex = -1;
 const lineHistory = [];
 
+/* ASCII Art*/
 var neofetch_data = `\r\n                  @@@@@@                    dev@EyeTrackVR\r\n              @@@@@@@@@@@            @@@    -------------- \r\n            @@@@@@@@@@@@      @@@@@@@@@@@   OS Arch Linux x86_64\r\n          @@@@@@@@@@@@@   @@@@@@@@@@@@@@    Host Your PC\r\n        @@@@@@@#         ,@@@@@@@@@@@@@     Kernel 5.5.13-arch1-1\r\n          ,@@@@@@@@@@@@@@@  @@@@@@@@        Uptime 69 days, 42 hours, 21 mins\r\n      @@@@@@@@@@@@@@@@@@@@@@@@ @@@@@        Shell bash 5.0.16\r\n  @@@@@@@@                @@@@@             CPU AMD Ryzen Threadripper PRO\r\n@@@                        @@@@@            Memory 3869MiB \/ 5229MiB\r\n              @@@@@@        @@@@            \r\n    @@@     @@@@@@@@@\/      @@@@@           \r\n    ,@@@.     @@@@@@((@     @@@@(           \r\n    \/\/@@@        *\/  @@@@  @@@@@            \r\n    @@@(                @@@@@@@             \r\n    @@@  @          @@@@@@@@@               \r\n        @@@@@@@@@@@@@@@@@                   \r\n        @@@@@@@@@@@@@&`;
+
+var contributors = [
+  `  _____           _                _       \r\n |  __ \\         | |              | |      \r\n | |__) | __ ___ | |__  _   _ _ __| |_ ____\r\n |  ___\/ \'__\/ _ \\| \'_ \\| | | | \'__| __|_  \/\r\n | |   | | | (_) | | | | |_| | |  | |_ \/ \/ \r\n |_|   |_|  \\___\/|_| |_|\\__,_|_|   \\__\/___|\r\n                                           `,
+  `   _____                                     \r\n  / ____|                                    \r\n | (___  _   _ _ __ ___  _ __ ___   ___ _ __ \r\n  \\___ \\| | | | '_ \` _ \\| '_ \` _ \\ / _ \\ '__|\r\n  ____) | |_| | | | | | | | | | | |  __/ |   \r\n |_____/ \\__,_|_| |_| |_|_| |_| |_|\\___|_|   \r\n                                             `,
+  `  ______                  _______ _    _ ______ _                \r\n |___  \/                 |__   __| |  | |  ____| |               \r\n    \/ \/ __ _ _ __  _____   _| |  | |__| | |__  | |__   __ _ _ __ \r\n   \/ \/ \/ _\` | \'_ \\|_  \/ | | | |  |  __  |  __| | \'_ \\ \/ _\` | \'__|\r\n  \/ \/_| (_| | | | |\/ \/| |_| | |  | |  | | |____| |_) | (_| | |   \r\n \/_____\\__,_|_| |_\/___|\\__, |_|  |_|  |_|______|_.__\/ \\__,_|_|   \r\n                        __\/ |                                    \r\n                       |___\/                                    `,
+  `  _                         \r\n | |                        \r\n | | ___  _ __ _____      __\r\n | |\/ _ \\| \'__\/ _ \\ \\ \/\\ \/ \/\r\n | | (_) | | | (_) \\ V  V \/ \r\n |_|\\___\/|_|  \\___\/ \\_\/\\_\/  \r\n                            \r\ `,
+  `            _       _     \r\n           | |     | |    \r\n   __ _  __| | ___ | |_   \r\n  \/ _\` |\/ _\` |\/ _ \\| __|  \r\n | (_| | (_| | (_) | |_   \r\n  \\__, |\\__,_|\\___\/ \\__|  \r\n     | |                  \r\n     |_|                  \r\n                          `,
+];
+/* End ASCII Art*/
 
 const terminal = new Terminal({
   theme: {
@@ -78,14 +88,40 @@ async function sendSerialLine() {
     dataToSend === "neofetch\r" ||
     dataToSend === "neofetch\n"
   )
-    printNeofetch();
+    printToConsole(neofetch_data, "34", false);
+  if (
+    dataToSend === "contributors" ||
+    dataToSend === "contributors\r\n" ||
+    dataToSend === "contributors\r" ||
+    dataToSend === "contributors\n"
+  )
+    printContributors();
   //await writer.write(dataToSend);
   document.getElementById("lineToSend").value = "";
   //await writer.releaseLock();
 }
 
-function printNeofetch() {
-  terminal.writeln(`\x1B[1;3;34m${neofetch_data}\x1B[0m`);
+function printToConsole(data, color, array) {
+  if (array == true) {
+    for (var i = 0; i < data.length; i++) {
+      terminal.writeln(`\x1B[1;3;${color}m${data[i]}\x1B[0m`);
+    }
+  } else {
+    terminal.writeln(`\x1B[1;3;${color}m${data}\x1B[0m`);
+  }
+}
+
+function printContributors() {
+  printToConsole(
+    "The Team:",
+    "34",
+    false
+  );
+  printToConsole(contributors[0], "34", false);
+  printToConsole(contributors[1], "33", false);
+  printToConsole(contributors[2], "32", false);
+  printToConsole(contributors[3], "31", false);
+  printToConsole(contributors[4], "37", false);
 }
 
 async function listenToPort() {
