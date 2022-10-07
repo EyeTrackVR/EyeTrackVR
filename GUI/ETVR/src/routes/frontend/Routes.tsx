ab99e50f321b93cab2399cf79ba575f6a5f0dfb7
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-10-07 16:28:05 +0100

    update - Remove old template styling and begin to add custom styling

--- GUI/ETVR/src/routes/frontend/Routes.tsx
+++ GUI/ETVR/src/routes/frontend/Routes.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AntxChatDesign01 from "../../pages/ETVRPage01";
-import AntxChatDesign02 from "../../pages/ETVRPage02";
+/* import AntxChatDesign02 from "../../pages/ETVRPage02"; */
 
 /* import.meta.domain_url = {
   PUBLIC_URL: "ETVR-Website"
@@ -14,7 +14,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<AntxChatDesign01 />} />
         <Route path="/home" element={<AntxChatDesign01 />} />
-        <Route path="/white-papers" element={<AntxChatDesign02 />} />
+        {/* <Route path="/white-papers" element={<AntxChatDesign02 />} /> */}
       </Routes>
     </BrowserRouter>
   );
