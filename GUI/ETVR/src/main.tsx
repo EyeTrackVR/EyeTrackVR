ee7d8bf71489a37f389aba377ffef9f3cc9db51d
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-10-08 17:24:58 +0100

    minor update - update splash screen to setTimeout for only 3 seconds instead of 5

--- GUI/ETVR/src/main.tsx
+++ GUI/ETVR/src/main.tsx
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   //sleep for 5 seconds to allow the window to load
   setTimeout(() => {
     invoke("close_splashscreen");
-  }, 5000);
+  }, 3000);
 });
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
