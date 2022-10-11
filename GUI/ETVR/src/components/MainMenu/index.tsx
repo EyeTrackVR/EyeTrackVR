69248befd77b37a99e20cce40b41e69041087530
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-10-11 16:24:06 +0100

    update begin adding menu features - adding CSS design for hover and target - adding logic and hooks for managing menu state

--- GUI/ETVR/src/components/MainMenu/index.tsx
+++ GUI/ETVR/src/components/MainMenu/index.tsx
@@ -1,19 +1,72 @@
+import * as React from "react";
 import { Logo } from "@src/components/Logo";
 import { Greeting } from "@src/components/Greeting";
 import styles from "./index.module.scss";
 
-const NavbarLinks = ["cameras", "settings"];
+/* function mapping() {
+  const navbarLinks = ["cameras", "settings"];
+  const map = navbarLinks.map((i) => {
+    return (
+      <div className={styles.navbar_links}>
+        <span>{i}</span>
+      </div>
+    );
+  });
+  return map;
+} */
 
 export function MainMenu() {
+  const [state, setState] = React.useState({
+    dashboard: true,
+    settings: false
+  });
+
+  const handleNavChange = (event) => {
+    setState({
+      ...state,
+      dashboard: !state.dashboard,
+      settings: !state.settings
+    });
+    console.log(event.currentTarget);
+  };
+
   return (
     <>
       <div className={styles.main_menu}>
         <Logo />
         <div className={styles.navbar_container}>
-          <nav className={styles.navbar_group}>
-            <div className={styles.rectangle_2}></div>
-            <div className={styles.active}>cameras</div>
-          </nav>
+          <div className={styles.navbar_group}>
+            <div onClick={handleNavChange} className={styles.navbar_item}>
+              <div
+                className={
+                  state.dashboard
+                    ? styles.dashboard_highlight
+                    : styles.highlight_inactive
+                }
+              ></div>
+              <div
+                className={state.dashboard ? styles.nav_item_active : styles.nav_item_inactive}
+              >
+                <span>dashboard</span>
+                {/* Dashboard Goes here */}
+              </div>
+            </div>
+          </div>
+          <div className={styles.navbar_group}>
+            <div onClick={handleNavChange} className={styles.navbar_item}>
+              <div
+                className={
+                  state.settings
+                    ? styles.settings_highlight
+                    : styles.highlight_inactive
+                }
+              ></div>
+              <div className={state.settings ? styles.nav_item_active : styles.nav_item_inactive}>
+                <span>settings</span>
+                {/* Settings Goes here */}
+              </div>
+            </div>
+          </div>
         </div>
         <div className={styles.rectangle_146}>
           <Greeting />
