c6e813cd0160d5c1f765174f45a531d45b0a66a2
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-11-11 03:36:17 +0000

    update - finish navbar design

--- GUI/ETVR/src/components/Header/index.tsx
+++ GUI/ETVR/src/components/Header/index.tsx
@@ -19,31 +19,75 @@ export default function Header(props) {
                 }}
             >
                 <div className="flex justify-around items-center">
-                    <div className="flex">
-                        <div className="">
-                            <img
-                                onClick={() => setShowSettings(!showSettings)}
-                                src={etvrLogo}
-                                alt="eytrackvrlogo"
-                                className="bg-gray-800
+                    <div className="flex-none">
+                        <img
+                            onClick={() => setShowSettings(!showSettings)}
+                            src={etvrLogo}
+                            alt="eytrackvrlogo"
+                            className="bg-gray-800
                                             hover:bg-gray-900 
                                             rounded-full
                                           focus:bg-gray-900 
                                             transition 
                                             duration-200 
-                                            ease-in 
+                                            ease-in
                                             focus:shadow-inner 
                                             w-[90px] shadow-lg"
-                            />
+                        />
+                    </div>
+                    <div
+                        className="flex
+                                   h-[55%]
+                                   content-center 
+                                   items-center 
+                                   mt-[5px]"
+                    >
+                        <div
+                            className="flex
+                                       flex-grow
+                                       justify-center
+                                       border-none
+                                       shadow-lg
+                                       content-center 
+                                       leading-5 
+                                       font-sans 
+                                       font-medium 
+                                       text-[.75rem]
+                                       rounded-[15px] 
+                                       h-[100%]
+                                       w-[100%]
+                                       bg-[#0e0e0e] 
+                                       text-[#5f5f5f]"
+                        >
+                            <div
+                                className="flex
+                                           flex-grow 
+                                           content-center
+                                           justify-evenly
+                                           h-[100%]
+                                           leading-5 
+                                           font-sans 
+                                           font-medium 
+                                           rounded-[14px]
+                                           p-[5px]
+                                           bg-[#0e0e0e]"
+                            >
+                                <div className="ml-[1.25rem] rounded-[8px] pt-[.2rem] pb-[.2rem] pl-[1.25rem] pr-[1.25rem] hover:bg-[#252536]">
+                                    <FontAwesomeIcon
+                                        size="xl"
+                                        icon={faCamera}
+                                    />
+                                </div>
+                                <div className="ml-[1.25rem] mr-[1.25rem] rounded-[8px] pt-[.2rem] pb-[.2rem] pl-[1.25rem] pr-[1.25rem] hover:bg-[#252536]">
+                                    <FontAwesomeIcon size="xl" icon={faGear} />
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div
                         className="flex 
                                    h-[55%] 
-                                   basis-[18%] 
-                                   justify-center 
                                    content-center 
-                                   items-center 
                                    mt-[5px]"
                     >
                         <div
@@ -54,12 +98,11 @@ export default function Header(props) {
                                        inset 
                                        shadow-lg 
                                        content-center 
-                                       leading-5 
+                                       leading-5
+                                       pl-[1rem] 
                                        font-sans 
                                        font-medium 
-                                       text-[.75rem] 
-                                       pl-[14px] 
-                                       mr-[-182px] 
+                                       text-[.75rem]
                                        rounded-[15px] 
                                        h-[100%] 
                                        bg-[#0e0e0e] 
@@ -72,74 +115,7 @@ export default function Header(props) {
                                            bg-[#0e0e0e] 
                                            flex-row 
                                            basis-[100%] 
-                                           justify-around 
-                                           content-stretch 
-                                           pt-[8px] 
-                                           pb-[8px] 
-                                           pr-[8px]"
-                            >
-                                <div
-                                    className="flex 
-                                                flex-row 
-                                                xl:space-x-24 
-                                                h-[100%] 
-                                                content-center 
-                                                leading-5 
-                                                font-sans 
-                                                font-medium text-[.75rem] 
-                                                justify-around 
-                                                basis-[100%]"
-                                >
-                                    <div className="pr-[6px]">
-                                        <FontAwesomeIcon
-                                            className="ml-8"
-                                            icon={faCamera}
-                                        />
-                                    </div>
-                                    <div className="pl-[6px]">
-                                        <FontAwesomeIcon
-                                            className="mr-8"
-                                            icon={faGear}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div
-                        className="flex 
-                                    h-[55%] 
-                                    basis-[18%] 
-                                    justify-around 
-                                    content-center 
-                                    mt-[5px]"
-                    >
-                        <div
-                            className="flex 
-                                       flex-row 
-                                       justify-start 
-                                       border-none 
-                                       inset 
-                                       shadow-lg 
-                                       content-center 
-                                       leading-5 
-                                       font-sans 
-                                       font-medium 
-                                       text-[.75rem] 
-                                       pl-[14px] 
-                                       mr-[-182px] 
-                                       rounded-[15px] h-[100%] 
-                                       bg-[#0e0e0e] 
-                                       text-[#5f5f5f]"
-                        >
-                            <div
-                                className="flex 
-                                           rounded-[14px] 
-                                           h-[100%] 
-                                           bg-[#0e0e0e] 
-                                           flex-row 
-                                           basis-[100%] 
-                                           justify-around 
+                                           justify-center 
                                            content-stretch 
                                            pt-[8px] 
                                            pb-[8px] 
--- GUI/ETVR/src/pages/Home/index.tsx
+++ GUI/ETVR/src/pages/Home/index.tsx
@@ -1,3 +1,3 @@
-export function Main(props) {
+export function Main() {
     return <></>;
 }
