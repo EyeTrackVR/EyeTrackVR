b28c16c9388b7cad70ebcbf053dbbf7f58d47858
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-11-11 01:43:28 +0000

    minor update - clean up organization of tailwind classes - set an example of how tailwind classes should be organized

--- GUI/ETVR/src/components/Header/index.tsx
+++ GUI/ETVR/src/components/Header/index.tsx
@@ -25,14 +25,71 @@ export default function Header(props) {
                                 onClick={() => setShowSettings(!showSettings)}
                                 src={etvrLogo}
                                 alt="eytrackvrlogo"
-                                className="bg-gray-800 hover:bg-gray-900 rounded-full focus:bg-gray-900 transition duration-200 ease-in focus:shadow-inner w-[90px] shadow-lg"
+                                className="bg-gray-800
+                                            hover:bg-gray-900 
+                                            rounded-full
+                                          focus:bg-gray-900 
+                                            transition 
+                                            duration-200 
+                                            ease-in 
+                                            focus:shadow-inner 
+                                            w-[90px] shadow-lg"
                             />
                         </div>
                     </div>
-                    <div className="flex h-[55%] basis-[18%] justify-center content-center items-center mt-[5px]">
-                        <div className="flex flex-row justify-start border-none inset shadow-lg content-center leading-5 font-sans font-medium text-[.75rem] pl-[14px] mr-[-182px] rounded-[15px] h-[100%] bg-[#0e0e0e] text-[#5f5f5f]">
-                            <div className="flex rounded-[14px] h-[100%] bg-[#0e0e0e] flex-row basis-[100%] justify-around content-stretch pt-[8px] pb-[8px] pr-[8px]">
-                                <div className="flex flex-row xl:space-x-24 h-[100%] leading-5 font-sans font-medium text-[.75rem] content-center justify-around basis-[100%]">
+                    <div
+                        className="flex 
+                                   h-[55%] 
+                                   basis-[18%] 
+                                   justify-center 
+                                   content-center 
+                                   items-center 
+                                   mt-[5px]"
+                    >
+                        <div
+                            className="flex 
+                                       flex-row 
+                                       justify-start 
+                                       border-none 
+                                       inset 
+                                       shadow-lg 
+                                       content-center 
+                                       leading-5 
+                                       font-sans 
+                                       font-medium 
+                                       text-[.75rem] 
+                                       pl-[14px] 
+                                       mr-[-182px] 
+                                       rounded-[15px] 
+                                       h-[100%] 
+                                       bg-[#0e0e0e] 
+                                       text-[#5f5f5f]"
+                        >
+                            <div
+                                className="flex 
+                                           rounded-[14px] 
+                                           h-[100%] 
+                                           bg-[#0e0e0e] 
+                                           flex-row 
+                                           basis-[100%] 
+                                           justify-around 
+                                           content-stretch 
+                                           pt-[8px] 
+                                           pb-[8px] 
+                                           pr-[8px]"
+                            >
+                                <div
+                                    className="flex 
+                                                flex-row 
+                                                xl:space-x-24 
+                                                h-[100%] 
+                                                content-center 
+                                                leading-5 
+                                                font-sans 
+                                                font-medium text-[.75rem] 
+                                                justify-around 
+                                                basis-[100%]"
+                                >
                                     <div className="pr-[6px]">
                                         <FontAwesomeIcon
                                             className="ml-8"
@@ -49,15 +106,56 @@ export default function Header(props) {
                             </div>
                         </div>
                     </div>
-                    <div className="flex h-[55%] basis-[18%] justify-around content-center mt-[5px]">
-                        <div className="flex flex-row justify-start border-none inset shadow-lg content-center leading-5 font-sans font-medium text-[.75rem] pl-[14px] mr-[-182px] rounded-[15px] h-[100%] bg-[#0e0e0e] text-[#5f5f5f]">
-                            <div className="flex rounded-[14px] h-[100%] bg-[#0e0e0e] flex-row basis-[100%] justify-around content-stretch pt-[8px] pb-[8px] pr-[8px]">
+                    <div
+                        className="flex 
+                                    h-[55%] 
+                                    basis-[18%] 
+                                    justify-around 
+                                    content-center 
+                                    mt-[5px]"
+                    >
+                        <div
+                            className="flex 
+                                       flex-row 
+                                       justify-start 
+                                       border-none 
+                                       inset 
+                                       shadow-lg 
+                                       content-center 
+                                       leading-5 
+                                       font-sans 
+                                       font-medium 
+                                       text-[.75rem] 
+                                       pl-[14px] 
+                                       mr-[-182px] 
+                                       rounded-[15px] h-[100%] 
+                                       bg-[#0e0e0e] 
+                                       text-[#5f5f5f]"
+                        >
+                            <div
+                                className="flex 
+                                           rounded-[14px] 
+                                           h-[100%] 
+                                           bg-[#0e0e0e] 
+                                           flex-row 
+                                           basis-[100%] 
+                                           justify-around 
+                                           content-stretch 
+                                           pt-[8px] 
+                                           pb-[8px] 
+                                           pr-[8px]"
+                            >
                                 <span className="text-[#5f5f5f]">
                                     {props.name}
                                 </span>
                             </div>
                             <FontAwesomeIcon
-                                className="object-cover mt-[3px] pt-[8px] pl-[2rem] pr-[1rem] text-[#f5f5f5]"
+                                className="object-cover 
+                                           mt-[3px] 
+                                           pt-[8px] 
+                                           pl-[2rem] 
+                                           pr-[1rem] 
+                                           text-[#f5f5f5]"
                                 icon={faChevronDown}
                             />
                         </div>
