54a241ce8a6a1e80630546a244f80dc4cdade0d0
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-11-24 20:03:17 +0000

    update - fix overflow on settings div - set settings div to automatically fill depending on size of window

--- GUI/ETVR/src/pages/Settings/index.tsx
+++ GUI/ETVR/src/pages/Settings/index.tsx
@@ -20,8 +20,8 @@ export function SettingsPage() {
   )
 
   return (
-    <div className="h-[100%] grow flex-col pt-6 py-6 px-8">
-      <Menu as="div" className="h-[100%]">
+    <div className="pb-[5rem] h-[95%] xl:h-[100%] xl:pb-[1rem] grow flex-col pt-6 py-6 px-8">
+      <Menu as="div" className="h-[95%] xl:h-[100%]">
         <div className="h-[95%] flex-1 grow rounded-[14px] border-solid border border-black shadow-lg leading-5 font-sans font-medium text-[.75rem]">
           <div className="h-[100%] flex-1 overflow-auto grow rounded-[14px] pr-1 bg-[#0f0f0f] pt-[.5rem] pb-[.5rem] text-[#ffffffc4]">
             <div className="flex flex-col divide-y divide-gray-800">
