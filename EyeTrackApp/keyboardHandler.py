970589acdca71efd214e43dca45092cf02c40abb
lorow <smykupyka@gmail.com>
2023-08-03 00:30:58 +0200

    Finally, implement PoC keyboard shortcuts handler that can be controlled by us

--- EyeTrackApp/keyboardHandler.py
+++ EyeTrackApp/keyboardHandler.py
@@ -1,28 +1,52 @@
+from functools import partial
+
 from pynput import keyboard
+
+from config import EyeTrackConfig
 from utils.eye_utils import trigger_recalibration, trigger_recenter
 
 
 class KeyboardHandler:
-
-    # TODO inherit after the GlobalHotkeys and setup your own threads with thread handling
-    def __init__(self, eye_widgets):
-        # since we're modifying the eye widgets settings directly
+    def __init__(self, eye_widgets, settings: EyeTrackConfig, event):
         self.eye_widgets = eye_widgets
-        self.shortcut_map = {
-            "settings-recalibrate": trigger_recalibration,
-            "settings-recenter": trigger_recenter,
+        self.settings = settings.settings
+        self.should_restart = False
+        self.event = event
+        self.listener = None
+
+    def _trigger_action(self, action):
+        for eye in self.eye_widgets:
+            action(eye)
+
+    def _get_shortcut_actions(self):
+        return {
+            self.settings.gui_reset_calibration_shortcut: partial(self._trigger_action, trigger_recalibration),
+            self.settings.gui_recenter_shortcut: partial(self._trigger_action, trigger_recenter)
         }
 
-    def process_shortcuts(self):
-        # TODO implement it, with settings and stuff
-        raise NotImplementedError
-        return {}
+    def _start(self):
+        shortcut_actions = self._get_shortcut_actions()
+        self.listener = keyboard.GlobalHotKeys(shortcut_actions)
+
+    def _restart(self):
+        self.should_restart = False
+        self._stop()
+        self.listener = None
+
+        self._start()
+
+    def _stop(self):
+        self.listener.stop()
+
+    def on_update(self):
+        # TODO figure out how to trigger this nicely, maybe observer?
+        # Would play nicely with what some users have been suggesting with changes taking immediate effect
+        self.should_restart = True
 
     def run(self):
-        # handle events here too
-        shortcuts = self.process_shortcuts()
+        self._start()
+        while not self.event.is_set():
+            if self.should_restart:
+                self._restart()
 
-        with keyboard.GlobalHotKeys(
-            **shortcuts,
-        ) as global_shortcuts:
-            global_shortcuts.join()
+        self._stop()
