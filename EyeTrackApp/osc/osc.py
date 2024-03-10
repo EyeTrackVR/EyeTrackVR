643f14831a54a82742004b71676329a758958922
lorow <smykupyka@gmail.com>
2024-03-10 21:42:38 +0100

    Add checkbox to switch to ETVR Module
    # TODO:
    # - there's ghosts in the machine - vrc osc is not working properly
    # - min/maxing will require field combinators in the modules lmao

--- EyeTrackApp/osc/osc.py
+++ EyeTrackApp/osc/osc.py
@@ -1,3 +1,4 @@
+from time import sleep
 from typing import Optional, Iterable, Callable
 
 from pythonosc import udp_client
@@ -12,33 +13,37 @@ import queue
 import threading
 
 
-# TODO
-# make gui
-
-
 class OSCManager:
     def __init__(
         self,
         osc_message_in_queue: queue.Queue[OSCMessage],
         config: EyeTrackConfig,
     ):
-        self.cancellation_event = threading.Event()
+        self.sender_cancellation_event = threading.Event()
+        self.receiver_cancellation_event = threading.Event()
         self.listeners = {}
         self.osc_message_in_queue = osc_message_in_queue
         self.config = config
         self.settings = config.settings
-        self.osc_sender: OSCSender = None
+        self.osc_sender: Optional[OSCSender] = None
         self.osc_receiver = None
         self.osc_sender_thread: Optional[threading.Thread] = None
         self.osc_receiver_thread: Optional[threading.Thread] = None
 
     def start(self):
-        self.osc_sender = OSCSender(self.cancellation_event, self.osc_message_in_queue, self.config)
+        self.setup_sender()
+        self.setup_receiver()
+
+    def setup_sender(self):
+        print(f"\033[92m[INFO] Setting up OSC sender\033[0m")
+        self.osc_sender = OSCSender(self.sender_cancellation_event, self.osc_message_in_queue, self.config)
         self.osc_sender_thread = threading.Thread(target=self.osc_sender.run)
         self.osc_sender_thread.start()
 
+    def setup_receiver(self):
         if self.settings.gui_ROSC:
-            self.osc_receiver = OSCReceiver(self.cancellation_event, self.config, self.listeners)
+            print(f"\033[92m[INFO] Setting up OSC receiver\033[0m")
+            self.osc_receiver = OSCReceiver(self.receiver_cancellation_event, self.config, self.listeners)
             self.osc_receiver_thread = threading.Thread(target=self.osc_receiver.run)
             self.osc_receiver_thread.start()
 
@@ -48,10 +53,36 @@ class OSCManager:
 
         self.listeners[osc_address].extend(callbacks)
 
+    def update(self, data: dict):
+        keys = set(data.keys())
+        sender_trigger_keys = {
+            "gui_osc_port",
+            "gui_PortNumber",
+            "gui_use_module",
+        }
+        if sender_trigger_keys.intersection(keys):
+            self.stop_sender()
+            self.setup_sender()
+
+        receiver_trigger_keys = {
+            "gui_ROSC",
+            "gui_osc_receiver_port",
+        }
+        if receiver_trigger_keys.intersection(keys):
+            self.stop_receiver()
+            self.setup_receiver()
+
     def shutdown(self):
-        self.cancellation_event.set()
+        self.stop_sender()
+        self.stop_receiver()
+
+    def stop_sender(self):
+        self.sender_cancellation_event.set()
         self.osc_sender_thread.join()
+
+    def stop_receiver(self):
         if self.osc_receiver_thread:
+            self.receiver_cancellation_event.set()
             self.osc_receiver_thread.join()
 
 
@@ -69,14 +100,17 @@ class OSCSender:
         self.vrc_sender = VRChatOSCSender()
         self.module_sender = VRCFTModuleSender()
 
-        # idea is, we will use a single port.
+        # idea is that we will use a single port.
         # if, the user selects in the settings that they want to use the module
         # we swap the port, and we're good
         # if they don't, we just output stuff as we'd normally
         # handlers are only here to handle different payloads pretty much
-        self.client = udp_client.SimpleUDPClient(self.config.gui_osc_address, int(self.config.gui_osc_port))
+        self.client = None
 
     def run(self):
+        osc_port = self.config.gui_osc_port if not self.config.gui_use_module else self.config.gui_PortNumber
+        self.client = udp_client.SimpleUDPClient(self.config.gui_osc_address, int(osc_port))
+
         while not self.cancellation_event.is_set():
             try:
                 osc_message: OSCMessage = self.msg_queue.get(block=True, timeout=0.1)
@@ -107,7 +141,12 @@ class OSCReceiver:
         self.cancellation_event = cancellation_event
         self.dispatcher = dispatcher.Dispatcher()
         self.listeners = listeners
+        self.server_thread = None
         try:
+            # this thing sucks ass god fucking damn it.
+            # like, there is no way of shutting it down UNLESS you run it in a thread
+            # which is kinda dumb, but oh well.
+            # Also, it doesn't shutdown properly. It's STILL connected to the port
             self.server = osc_server.OSCUDPServer(
                 (self.config.gui_osc_address, int(self.config.gui_osc_receiver_port)),
                 self.dispatcher,
@@ -119,6 +158,7 @@ class OSCReceiver:
         print("\033[94m[INFO] Exiting OSC Receiver\033[0m")
         try:
             self.server.shutdown()
+            self.server_thread.join()
         except Exception:  # noqa
             pass
 
@@ -130,6 +170,12 @@ class OSCReceiver:
         try:
             self.dispatcher.set_default_handler(self.handle_osc_message)
             print("\033[92m[INFO] OSC Listening on {}\033[0m".format(self.server.server_address))
-            self.server.serve_forever()
+            self.server_thread = threading.Thread(target=self.server.serve_forever)
+            self.server_thread.start()
+
+            while not self.cancellation_event.is_set():
+                sleep(10)
+
+            self.shutdown()
         except Exception:  # noqa:
             print(f"\033[91m[ERROR] OSC Receive port: {self.config.gui_osc_receiver_port} occupied.\033[0m")
