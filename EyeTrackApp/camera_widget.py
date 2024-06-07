7cca1cb7658dd4cd821650e4b6c65ceec49c3554
Lorow <goik.zdzislaw@gmail.com>
2024-06-08 00:21:58 +0200

    Add a TODO, probably to be ignored

--- EyeTrackApp/camera_widget.py
+++ EyeTrackApp/camera_widget.py
@@ -367,6 +367,7 @@ class CameraWidget:
 
             self.x1, self.y1 = values[self.gui_roi_selection]
 
+        # todo, this is now kinda duplicated with the OSC implementation
         if event == self.gui_restart_3d_calibration:
             self.ransac.calibration_3d_frame_counter = -621
             self.settings.gui_3d_calibration = True
