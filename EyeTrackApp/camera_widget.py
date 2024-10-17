7643d7c2345c87911ae6fec2454bfaee491556e5
Fynnley <86367070+FynnleyNeko@users.noreply.github.com>
2024-10-17 20:27:02 +0200

    Add ASeeVR support to camera_widget.py
    This adds "aseevr" to the http autocomplete exceptions so the GUI doesn't mess with entering "aseevrleft" or "aseevrright" as the camera.

--- EyeTrackApp/camera_widget.py
+++ EyeTrackApp/camera_widget.py
@@ -379,6 +379,7 @@ class CameraWidget:
                         if (
                             len(values[self.gui_camera_addr]) > 5
                             and "http" not in values[self.gui_camera_addr]
+                            and "aseevr" not in values[self.gui_camera_addr]
                             and ".mp4" not in values[self.gui_camera_addr]
                         ):  # If http is not in camera address, add it.
                             self.config.capture_source = f"http://{values[self.gui_camera_addr]}/"
