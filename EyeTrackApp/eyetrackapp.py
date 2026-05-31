19eb333c38cf5276e8970218d493e5a712c39c4d
Prohurtz <48768484+RedHawk989@users.noreply.github.com>
2026-05-30 20:08:54 -0700

    bump ver

--- EyeTrackApp/eyetrackapp.py
+++ EyeTrackApp/eyetrackapp.py
@@ -57,7 +57,7 @@ from utils.tooltips import attach_tooltip
 
 
 
-APP_VERSION = "EyeTrackApp 0.3.0 BETA 4.1"
+APP_VERSION = "EyeTrackApp 0.3.0 BETA 5"
 setup_logging(APP_VERSION)
 logger = logging.getLogger(__name__)
 winmm = None
